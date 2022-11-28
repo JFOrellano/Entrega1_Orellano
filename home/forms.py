505832f6b931e2d15659f1156013341fe23217bd
Mario Reggiardo <marioreggiardo24@gmail.com>
2022-11-28 11:45:35 -0300

    EditUser

--- home/forms.py
+++ home/forms.py
@@ -32,9 +32,11 @@ class UserEditrForm(UserCreationForm):
     password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
     password2 = forms.CharField(label="Repetir contraseña", widget=forms.PasswordInput)
 
+    last_name = forms.CharField()
+    first_name = forms.CharField() 
 class Meta:
     model = User
-    fields = ['username', 'email', 'password1', 'password2']
+    fields = ['username', 'email', 'password1', 'password2', 'last_name', 'first_name']
  
     help_texts = {k:"" for k in fields}
 
