5ccd3534aa76c724fff9b9209c042deef61f3acd
Mario Reggiardo <marioreggiardo24@gmail.com>
2022-11-28 09:27:54 -0300

    Entrega lista

--- home/urls.py
+++ home/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("search/", views.search, name="search"),
     path('login', views.login_request, name='login'),
-    path('register', views.register, name='Register'),
+    path('registracion', views.register, name='Register'),
     path('logout', LogoutView.as_views(template_name='home/logout.html'), name='logout'),
     path('editarPerfil',views.editarPerfil,name='EditarPerfil'),
 ]
