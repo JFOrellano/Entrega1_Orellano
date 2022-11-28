12d7ff1faa32166233a4ac0c0af36ec1af2deb52
JFOrellano <108527256+JFOrellano@users.noreply.github.com>
2022-11-28 08:46:58 -0300

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("search/", views.search, name="search"),
     path("about/", views.about, name="about"),
+    path('login', views.login_request, name='login'),
+    path('register', views.register, name='Register'),
+    path('logout', LogoutView.as_views(template_name='home/logout.html'), name='logout'),
+    path('editarPerfil',views.editarPerfil,name='EditarPerfil'),
 ]
