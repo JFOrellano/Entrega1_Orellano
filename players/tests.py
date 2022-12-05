9c85ce303066c78c0a61e2d13e002a828fe26997
Orellano Julio <orellano.jf@gmail.com>
2022-12-05 19:23:37 -0300

    JOrellano Tests

--- players/tests.py
+++ players/tests.py
@@ -11,8 +11,8 @@ class PlayerTestCase(TestCase):
             username="testuser",
             password="12345",
         )
-        Player.objects.create(name="Python", code=123, owner=self.test_user)
-        Player.objects.create(name="Docker", code=789, owner=self.test_user)
+        Player.objects.create(name="Julio", last_name="Orellano", team="equipo1", number=11, position="atacante", owner=self.test_user)
+        Player.objects.create(name="Mario", last_name="Reggiardo", team="equipo2", number=11, position="atacante", owner=self.test_user)
 
         player_test_num = 20
         self.mock_names = [
@@ -24,7 +24,25 @@ class PlayerTestCase(TestCase):
             )
             for _ in range(player_test_num)
         ]
-        self.mock_codes = [
+        self.mock_last_names = [
+            "".join(
+                [
+                    random.choice((string.ascii_letters + string.digits))
+                    for _ in range(random.randint(4, 20))
+                ]
+            )
+            for _ in range(player_test_num)
+        ]
+        self.mock_teams = [
+            "".join(
+                [
+                    random.choice((string.ascii_letters + string.digits))
+                    for _ in range(random.randint(4, 20))
+                ]
+            )
+            for _ in range(player_test_num)
+        ]
+        self.mock_numbers = [
             int(
                 "".join(
                     [
@@ -35,21 +53,33 @@ class PlayerTestCase(TestCase):
             )
             for _ in range(player_test_num)
         ]
+        self.mock_positions = [
+            "".join(
+                [
+                    random.choice((string.ascii_letters + string.digits))
+                    for _ in range(random.randint(4, 20))
+                ]
+            )
+            for _ in range(player_test_num)
+        ]
 
-        for mock_name, mock_code in zip(self.mock_names, self.mock_codes):
-            Player.objects.create(name=mock_name, code=mock_code, owner=self.test_user)
+        for mock_name, mock_last_name, mock_team, mock_number, mock_position in zip(self.mock_names, self.mock_last_names, self.mock_teams, self.mock_numbers, self.mock_positions):
+            Player.objects.create(name=mock_name, last_name=mock_last_name, team=mock_team, number=mock_number, position=mock_position, owner=self.test_user)
 
     def test_player_model(self):
         """Players creation are correctly identified"""
-        python_player = Player.objects.get(name="Python")
-        docker_player = Player.objects.get(name="Docker")
+        python_player = Player.objects.get(name="Julio")
+        docker_player = Player.objects.get(name="Mario")
         self.assertEqual(python_player.owner.username, "testuser")
         self.assertEqual(docker_player.owner.username, "testuser")
-        self.assertEqual(python_player.code, 123)
-        self.assertEqual(docker_player.code, 789)
+        self.assertEqual(python_player.last_name, "Orellano")
+        self.assertEqual(docker_player.last_name, "Reggiardo")
 
     def test_player_list(self):
-        for mock_name, mock_code in zip(self.mock_names, self.mock_codes):
+        for mock_name, mock_last_name, mock_team, mock_number, mock_position in zip(self.mock_names, self.mock_last_names, self.mock_teams, self.mock_numbers, self.mock_positions):
             player_test = Player.objects.get(name=mock_name)
             self.assertEqual(player_test.owner.username, "testuser")
-            self.assertEqual(player_test.code, mock_code)
+            self.assertEqual(player_test.last_name, mock_last_name)
+            self.assertEqual(player_test.team, mock_team)
+            self.assertEqual(player_test.number, mock_number)
+            self.assertEqual(player_test.position, mock_position)
